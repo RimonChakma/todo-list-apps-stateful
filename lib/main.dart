a3352a9f964bc5c3cec0779071eb5aa9ab18e14e
RimonChakma <rimon.chakma880@gmail.com>
2025-03-14 23:17:46 +0600

    basic list app structure

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ListScreen(),
+    );
+  }
+}
+
+class ListScreen extends StatefulWidget {
+  const ListScreen({super.key});
+
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Todo List Apps"),),
+    );
+  }
+}
+
